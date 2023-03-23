d29e7c16b7812f55b155e50535d1ee65a1c733ce
Simon Jønsson <sj@morningtrain.dk>
2023-03-23 12:18:46 +0100

    ✨ Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'brand': '#bf1db4',
+      },
+      colors: {
+        pink: {
+          '800': '#8c0f75',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
